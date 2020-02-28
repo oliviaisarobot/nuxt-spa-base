f0a1c56e93956994aaa3d8d7401bd82d46d62fb5
Olivia Gardiner <oliv.gardiner@gmail.com>
2020-02-28 21:56:41 +0100

    Maintenance layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,118 @@
 <template lang="pug">
   div.layout
+    app-header
+    app-to-the-top
     nuxt#content
+    app-footer
 </template>
 
 <script>
-export default {}
+import AppFooter from '~/components/app/Footer.vue'
+import AppHeader from '~/components/app/Header.vue'
+import AppToTheTop from '~/components/app/ToTheTop.vue'
+
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AppToTheTop
+  },
+  data() {
+    return {
+      blocks: []
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      this.$store.commit('browser/setBrowserSize')
+    }
+  }
+}
 </script>
 
 <style lang="scss">
 @import '~/assets/styles/style.scss';
+
+html,
+body,
+.layout {
+  background-color:                 $color-background;
+  box-sizing:                       border-box;
+  color:                            $font-color-dark;
+  font-family:                      $base-font-family;
+  font-size:                        $font-size * 0.8;
+  margin:                           0;
+  overflow-x:                       hidden;
+  padding:                          0;
+  position:                         relative;
+  word-spacing:                     1px;
+  -ms-text-size-adjust:             100%;
+  -webkit-text-size-adjust:         100%;
+  -moz-osx-font-smoothing:          grayscale;
+  -webkit-font-smoothing:           antialiased;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing:                       border-box;
+  margin:                           0;
+  padding:                          0;
+}
+
+a,
+a:hover,
+a:focus,
+button,
+button:hover,
+button:focus,
+li,
+li:hover,
+li:focus {
+  @include transition;
+}
+
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1.7rem;
+  font-weight: bold;
+}
+
+h3 {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+h4 {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+h5 {
+  font-size: 1.25rem;
+  font-weight: normal;
+}
+
+hr {
+  border-top:                       1px solid $color-primary!important;
+  opacity:                          .5;
+}
+
+.container {
+  padding: 0 2em 0 2em;
+}
+
+.cursor-pointer {
+  cursor:                           pointer;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,71 @@
 <template lang="pug">
-  div.text-center.pt-10 Under maintenance. Check back later!
+  div
+    app-page-heading.index-heading
+      template(slot="heading")
+        h1.text-white {{ $t('placeholders.header') }}
+        div.text-white {{ $t('placeholders.paragraph') }}
+    block-tiles(:tiles="tilesOne").mt-8.mb-8.pt-8.pb-8
+    hr.container
+    block-content.mt-8.mb-8.pt-8.pb-8
+      template(slot="first")
+        p {{ $t('placeholders.paragraph') }}
+      template(slot="second")
+        p {{ $t('placeholders.paragraph') }}
+    block-tiles(:tiles="tilesTwo", :fade-images="true").mt-8.mb-8.pt-8.pb-8
+      template(slot="tile-header")
+        h2 {{ $t('placeholders.header') }}
 </template>
 
 <script>
-export default {}
+import AppPageHeading from '~/components/app/PageHeading'
+import BlockContent from '~/components/block/Content'
+import BlockSlider from '~/components/block/Slider'
+import BlockTiles from '~/components/block/Tiles'
+
+export default {
+  layout: 'maintenance',
+  components: {
+    AppPageHeading,
+    BlockContent,
+    BlockSlider,
+    BlockTiles
+  },
+  computed: {
+    tilesOne() {
+      return [
+        { title: this.$t('placeholders.header'), text: this.$t('placeholders.paragraph-short'), img: require('~/assets/images/blank-image.svg') },
+        { title: this.$t('placeholders.header'), text: this.$t('placeholders.paragraph-short'), img: require('~/assets/images/blank-image.svg') },
+        { title: this.$t('placeholders.header'), text: this.$t('placeholders.paragraph-short'), img: require('~/assets/images/blank-image.svg') }
+      ]
+    },
+    tilesTwo() {
+      return [
+        { title: '', text: '', img: require('~/assets/images/blank-image.svg') },
+        { title: '', text: '', img: require('~/assets/images/blank-image.svg') },
+        { title: '', text: '', img: require('~/assets/images/blank-image.svg') },
+        { title: '', text: '', img: require('~/assets/images/blank-image.svg') },
+        { title: '', text: '', img: require('~/assets/images/blank-image.svg') }
+      ]
+    },
+    slides() {
+      return [
+        { title: this.$t('placeholders.header') + ' 1', text: this.$t('placeholders.paragraph-short'), img: '' },
+        { title: this.$t('placeholders.header') + ' 2', text: this.$t('placeholders.paragraph-short'), img: '' },
+        { title: this.$t('placeholders.header') + ' 3', text: this.$t('placeholders.paragraph-short'), img: '' }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss">
 @import '~/assets/styles/style.scss';
+
+.index-heading::before {
+  background-image: url('../assets/photos/home-office-1867761_1920_Pixabay_by_Pexels.jpg');
+}
+
+.second-banner::before {
+  background-image: url('../assets/photos/pen-4163403_1920_Pixabay_by_Samuel_F_Johanns.jpg');
+}
 </style>
