4401e67c904b643012a363ec3d9cc5266f5385c1
Olivia Gardiner <oliv.gardiner@gmail.com>
2020-02-28 20:49:30 +0100

    Maintenance layout

---
+++ layouts/maintenance.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div.flex.items-center.justify-stretch.h-screen
+    h1.text-center.pb-10.pt-10.bg-white.w-full Under maintenance!
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/style.scss';
+
+html, body {
+  background: repeating-linear-gradient(
+    -45deg,
+    black,
+    black 30px,
+    yellow 30px,
+    yellow 50px
+  );
+  height: 100vh;
+  margin: 0;
+  width: 100vw;
+}
+</style>
